6a44c203548aa15d563e281f1cc85dffcc8a65d6
SNOWIK1 <dimais444@mail.ru>
2024-03-05 00:10:26 +0700

    feat: catalog.css catalog.ejs

--- backend/index.ts
+++ backend/index.ts
@@ -8,13 +8,17 @@ import { ProductModel } from "./db/models/product";
 import { UserModel } from "./db/models/user";
 import { AppTypes } from "./types";
 
-
 const PORT: number = parseInt(process.env.PORT ?? "3000")
 
 fastify.get("/", async (req: FastifyRequest, rep: FastifyReply) => {
     console.log(req.hostname)
-    return rep.view("/catalog.ejs", {})
+    return rep.view("/index.ejs", {})
 })
+
+fastify.get("/catalog", async (req: FastifyRequest, rep: FastifyReply) => {
+  console.log(req.hostname);
+  return rep.view("/catalog.ejs", {products: []});
+});
 
 fastify.listen({ port: PORT}, function (err, address) {
   if (err) {
